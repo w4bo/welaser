68961d3680bab7f0e3330047ddb8a78599935afc
Your Name <you@example.com>
2022-03-31 11:36:46 +0000

    working on build

--- demo7-replay/scripts/cleanEnv.sh
+++ demo7-replay/scripts/cleanEnv.sh
@@ -18,7 +18,7 @@ sed -i 's/'${PROXY_IP}'/'${FAKEIP}'/g' .env.example
 sed -i 's/MOSQUITTO_USER=.*\s*/MOSQUITTO_USER=foo/g' .env.example
 sed -i 's/MOSQUITTO_PWD=.*\s*/MOSQUITTO_PWD=bar/g' .env.example
 sed -i 's/USER=.*\s*/USER=foo/g' .env.example
-sed -i 's/FIWARE_API_KEY=.*\s*/FIWARE_API_KEY=4jggokgpepnvsb2uv4s40d59ov/g' .env.example
+sed -i 's/FIWARE_API_KEY=.*\s*/FIWARE_API_KEY=123/g' .env.example
 sed -i 's/FIWARE_SERVICE=.*\s*/FIWARE_SERVICE=openiot/g' .env.example
 sed -i 's/FIWARE_SERVICEPATH=.*\s*/FIWARE_SERVICEPATH=\//g' .env.example
 sed -i 's/CODE_FOLDER=.*\s*/CODE_FOLDER=/path/to/code/here/g' .env.example
