4f9bc4f85712c48fde0b08666f3ac52ccd5e0582
Matteo Francia <m.francia@unibo.it>
2022-03-31 13:15:19 +0200

    add semantic release bot

---
+++ release.config.js
@@ -0,0 +1,18 @@
+var publishCmd = `
+git tag -a -f \${nextRelease.version} \${nextRelease.version} -F CHANGELOG.md
+git push --force origin \${nextRelease.version}
+`
+var config = require('semantic-release-preconfigured-conventional-commits');
+config.plugins.push(
+    ["@semantic-release/exec", {
+        "publishCmd": publishCmd,
+    }],
+    ["@semantic-release/github", {
+        "assets": [
+          {"path": "slides.zip"} // , "label": "Slides",
+          // {"path": "exercises/", "label": "DB exercises"}
+        ]
+    }],
+    "@semantic-release/git",
+)
+module.exports = config
