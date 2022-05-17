74f06d43998889080aef94761641244beb1ab403
w4bo <m.francia@unibo.it>
2022-05-17 17:44:38 +0200

    working on... but tests are broken

--- welaser/devices/src/main/kotlin/it/unibo/devices/MissionSimulator.kt
+++ welaser/devices/src/main/kotlin/it/unibo/devices/MissionSimulator.kt
@@ -47,5 +47,5 @@ fun main(args: Array<String>) {
                 DeviceMQTT(true, timeStamp(), false, 40.31285012589443, -3.4811514708229670, domain, mission, s3, p3),
                 DeviceMQTT(true, timeStamp(), false, 40.31184130935516, -3.4810637987225532, domain, mission, s4, p4),
             )
-    ).forEach { d -> d.run() } // .forEach { d -> executor.submit { d.run() } }
+    ).forEach { d -> executor.submit { d.run() } } // .forEach { d -> d.run() } //
 }
