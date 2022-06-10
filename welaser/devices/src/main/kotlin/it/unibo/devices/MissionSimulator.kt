58096c2e2afc7b42661b440f532340fd4006cfc0
w4bo <m.francia@unibo.it>
2022-06-10 12:47:23 +0200

    getting back an MQTT device

--- welaser/devices/src/main/kotlin/it/unibo/devices/MissionSimulator.kt
+++ welaser/devices/src/main/kotlin/it/unibo/devices/MissionSimulator.kt
@@ -3,11 +3,17 @@
 package it.unibo.devices
 
 import it.unibo.ROBOT_CMD_START
+import kotlinx.cli.ArgParser
+import kotlinx.cli.ArgType
+import kotlinx.cli.default
 import java.util.concurrent.Executors
 
 fun main(args: Array<String>) {
     val timeout = 1000
     val folder = "/datamodels"
+    val parser = ArgParser("MissionSimulator")
+    val domain by parser.option(ArgType.String, shortName = "domain", description = "Domain name").default(DOMAIN)
+    val mission by parser.option(ArgType.String, shortName = "mission", description = "Mission name").default(MISSION)
     EntityFactory.createFromFile("$folder/mission-123.json", 1, 2).run()
     val robot = EntityFactory.createFromFile("$folder/carob-1.json", timeout)
     robot.exec(ROBOT_CMD_START, "mission-123")
@@ -15,9 +21,9 @@ fun main(args: Array<String>) {
     (
             listOf(
                 robot,
-                // DeviceMQTT(STATUS.ON, timeStamp(), false, 40.31308266787424, -3.4804348644627585, domain, mission, Thermometer()),
-                // DeviceMQTT(STATUS.ON, timeStamp(), false, 40.31285012589443, -3.4811514708229670, domain, mission, Thermometer()),
-                // DeviceMQTT(STATUS.ON, timeStamp(), false, 40.31184130935516, -3.4810637987225532, domain, mission, Thermometer()),
+                // DeviceMQTT(STATUS.ON, timeStamp(), false, 40.31308266787424, -3.4804348644627585, domain, mission, RandomSensor()),
+                // DeviceMQTT(STATUS.ON, timeStamp(), false, 40.31285012589443, -3.4811514708229670, domain, mission, RandomSensor()),
+                DeviceMQTT(STATUS.ON, timeStamp(), false, 40.31184130935516, -3.4810637987225532, domain, mission, RandomSensor()),
                 // DeviceMQTT(STATUS.ON, timeStamp(), false, 40.31231176524012, -3.4810422377848910, domain, mission, Camera()),
                 EntityFactory.createFromFile("$folder/weatherstation-1.json", timeout),
                 EntityFactory.createFromFile("$folder/camera-1.json", timeout * 3),
