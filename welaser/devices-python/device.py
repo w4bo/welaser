6f20ca0a56cabb7df682bfd8b3ddba9e16aa4eaa
w4bo <m.francia@unibo.it>
2022-06-10 13:53:20 +0200

    update the robot with image encoding

--- welaser/devices-python/device.py
+++ welaser/devices-python/device.py
@@ -3,12 +3,13 @@ import os
 from dotenv import load_dotenv, find_dotenv
 import json
 import time
+import base64
 
 # load the environment variables
 load_dotenv(find_dotenv())
 
 # read the initial status from file
-with open("carob-123.json") as f:
+with open("carob-1.json") as f:
     status = json.load(f)
     status["id"] = "carob-python"
 
@@ -31,8 +32,16 @@ i = 0
 while i < 300:
     # sleep
     time.sleep(1)
-    # update the entity
+    # update the entity status
     status["status"][2] = time.time()
+    # ... and image
+    with open("img01.png", "rb") as image_file:
+        # You cannot send messages containing "=" to the OCB,
+        # so you need to encode it using its html representation ("%3D")
+        # See the encoding here: https://www.w3schools.com/tags/ref_urlencode.asp
+        status["status"][1] = base64.b64encode(image_file.read())
+        status["status"][1] = status["status"][1].decode('utf-8').replace("=", "%3D")
+
     # update the context broker
     r = requests.patch(url = "http://{}:{}/v2/entities/{}/attrs?options=keyValues".format(os.environ.get("ORION_IP"), os.environ.get("ORION_PORT_EXT"), id),
                    data = json.dumps(status),
