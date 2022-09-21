38fc03050ad381fedbc8048909f58004aaacbc90
w4bo <you@example.com>
2022-09-21 09:52:38 +0000

    updating charts

--- welaser/devices/src/main/kotlin/it/unibo/devices/ScalabilityTest.kt
+++ welaser/devices/src/main/kotlin/it/unibo/devices/ScalabilityTest.kt
@@ -6,11 +6,12 @@ import java.util.concurrent.Executors
 import java.util.concurrent.TimeUnit
 
 fun main(args: Array<String>) {
-    listOf(1000 * 60 * 200).forEach { duration -> /* 200 minutes */
+    listOf(1000 * 60 * 60 * 24 * 2).forEach { duration -> /* 1000 * 60 * 60 * 24 * 2 = 2 days, 1000 * 60 * 200 = 200 minutes */
         listOf(100).forEach { devices -> /* 100 devices */
             listOf(1).forEach { frequency -> /* 1 message every second */
                 listOf(0).forEach { payload -> /* 0B of additional payload */
                     val mission = "TEST--dev-$devices--freq-$frequency--pay-$payload--dur-$duration"
+                    println("Start. $mission")
                     val executor = Executors.newCachedThreadPool()
                     val periodMs = 1000 / frequency
                     (1..devices).map {
