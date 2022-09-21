889791deba87470aa7391a468a950ed1b44befdc
w4bo <you@example.com>
2022-09-21 15:32:39 +0000

    sleep after composing kafka rather than within the single services connecting to kafka

--- welaser/service-writetomongo/write.py
+++ welaser/service-writetomongo/write.py
@@ -1,13 +1,9 @@
-from kafka import KafkaConsumer
-from pymongo import MongoClient
-from time import sleep
 import json
 import os
 import time
 import uuid
-
-# Wait for kafka to be up and running
-sleep(30)
+from kafka import KafkaConsumer
+from pymongo import MongoClient
 
 KAFKA_IP = os.getenv("KAFKA_IP")
 KAFKA_PORT = os.getenv("KAFKA_PORT_EXT")
@@ -19,7 +15,7 @@ MONGO_CONNECTION_STR = "mongodb://{}:{}".format(MONGO_IP, MONGO_PORT)
 
 consumer = KafkaConsumer(
     DRACO_RAW_TOPIC,
-    group_id = str(uuid.uuid1()),
+    group_id=str(uuid.uuid1()),
     bootstrap_servers=[KAFKA_IP + ":" + KAFKA_PORT],
     value_deserializer=lambda x: json.loads(x.decode('utf-8'))
 )
