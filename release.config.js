6763795ec0265e79d83296b6ae85913d08cc2764
w4bo <you@example.com>
2022-09-22 07:42:26 +0000

    add charts to the release

--- release.config.js
+++ release.config.js
@@ -1,15 +1,15 @@
-var publishCmd = `
+const publishCmd = `
 git tag -a -f \${nextRelease.version} \${nextRelease.version} -F CHANGELOG.md
 git push --force origin \${nextRelease.version}
-`
-var config = require('semantic-release-preconfigured-conventional-commits');
+`;
+const config = require('semantic-release-preconfigured-conventional-commits');
 config.plugins.push(
     ["@semantic-release/exec", {
         "publishCmd": publishCmd,
     }],
     ["@semantic-release/github", {
         "assets": [
-          // {"path": "exercises/", "label": "DB exercises"}
+            {"path": "welaser/testScripts/*.pdf", "label": "Charts"}
         ]
     }],
     "@semantic-release/git",
