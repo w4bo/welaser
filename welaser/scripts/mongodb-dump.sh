3afa2ab90dee09ff5a63852743fb94a39b9c2631
w4bo <you@example.com>
2022-09-28 14:20:42 +0000

    test the database dump

--- welaser/scripts/mongodb-dump.sh
+++ welaser/scripts/mongodb-dump.sh
@@ -7,7 +7,7 @@ else
   echo "Could not find the .env file"
   exit 1
 fi
-
-docker exec -i mongo-db /usr/bin/mongodump --port ${MONGO_DB_PERS_PORT_INT} --db ${MONGO_DB_PERS_DB} --out /dump
-mkdir -p mongodb-dump
-docker cp mongo-db:/dump mongodb-dump
+D=$(date +%s)
+docker exec -i mongo-db /usr/bin/mongodump --port ${MONGO_DB_PERS_PORT_INT} --db ${MONGO_DB_PERS_DB} --out /dump-${D}
+mkdir -p mongodb-dump/
+docker cp mongo-db:/dump-${D} mongodb-dump
