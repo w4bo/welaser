e6341e2624ab30c3d419fe18fe745a87dcc70f52
w4bo <you@example.com>
2022-09-29 07:36:31 +0000

    also restore the database

--- welaser/scripts/mongodb-dump.sh
+++ welaser/scripts/mongodb-dump.sh
@@ -7,7 +7,9 @@ else
   echo "Could not find the .env file"
   exit 1
 fi
-D=$(date +%s)
+# D=$(date +%s)
+D=$(date +"%Y_%m_%d_%H_%M_%S")
 docker exec -i mongo-db /usr/bin/mongodump --port ${MONGO_DB_PERS_PORT_INT} --db ${MONGO_DB_PERS_DB} --out /dump-${D}
 mkdir -p mongodb-dump/
 docker cp mongo-db:/dump-${D} mongodb-dump
+docker exec -i mongo-db /usr/bin/mongorestore --nsFrom ${MONGO_DB_PERS_DB}'.*' --nsTo ${MONGO_DB_PERS_DB}-${D}'.*' /dump-${D}
