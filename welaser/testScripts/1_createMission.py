5be70d5d5eefd8238aef10e5bf697867db3b8544
w4bo <you@example.com>
2022-10-11 06:56:25 +0000

    working on the tests

--- welaser/testScripts/1_createMission.py
+++ welaser/testScripts/1_createMission.py
@@ -101,12 +101,10 @@ MONGO_IP = conf["MONGO_DB_PERS_IP"]
 MONGO_PORT = conf["MONGO_DB_PERS_PORT_EXT"]
 MONGO_CONNECTION_STR = "mongodb://{}:{}".format(MONGO_IP, MONGO_PORT)
 client = MongoClient(MONGO_CONNECTION_STR)  # connect to mongo
-count1 = len(list(client[conf["MONGO_DB_PERS_DB"]][domain].find()))
-# count2 = count1
+count1 = 0
 i = 0
 while i < 50 and count1 == 0:
     time.sleep(1)
-    # count2 = len(list(client[conf["MONGO_DB_PERS_DB"]][domain].find()))
+    count1 = len(list(client[conf["MONGO_DB_PERS_DB"]][domain].find()))
     i += 1
-# assert count2 > count1, "No new document found"
 assert count1 > 0, "No new document found"
