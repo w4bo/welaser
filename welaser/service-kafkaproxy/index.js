1fa9b1c19b7a239d9b2bc8ea5ffad81bb40fdcce
w4bo <you@example.com>
2022-10-12 08:52:21 +0000

    reworking the kafka proxy

--- welaser/service-kafkaproxy/index.js
+++ welaser/service-kafkaproxy/index.js
@@ -6,7 +6,7 @@ const env = require('dotenv')
 const io = require('socket.io')(server);
 const {Kafka, logLevel} = require("kafkajs")
 const uuid = require('uuid');
-app.use(cors({origin: '*'}))
+app.use(cors({origin: '*'})) // enable cross domain requests
 app.use(express.json())
 env.config() // load the environment variables
 const kafkaBrokers = [process.env.KAFKA_IP + ":" + process.env.KAFKA_PORT_EXT]
@@ -17,48 +17,55 @@ const kafka = new Kafka({
 })
 const producerKafka = kafka.producer()
 producerKafka.connect()
+
+/*
+ * General idea:
+ * - Cache a Kafka consumer for every topic requested through "newtopic" (i.e., one consumer per topic)
+ * - Each socket will join the "newtopic" room and leave the previous room (if any)
+ * - Kafka consumer broadcast messages to their respective rooms
+ */
+const consumers = {}
+
 io.on("connection", function (socket) {
-    let consumer = undefined
     let prevTopic = undefined
     socket.on("publish", function (data) { // when the socket receives a message with topic "publish"
         producerKafka.send({topic: data.topic, messages: [{value: JSON.stringify(data.data)}]}) // send it to kafka
     })
     socket.on("newtopic", function (topic) { // when the socket receives a message with topic "newtopic"
-        if (topic == prevTopic) return // if the topic is the same as before do nothing
+        if (typeof consumers[topic] === "undefined") { // if there is no costumer for the requested topic...
+            const groupId = topic + ".group." + uuid.v4() // create a unique consumer group
+            console.log("Registering a new Kafka consumer with group: " + groupId)
+            const consumer = kafka.consumer({groupId}) // initialize the consumer
+            consumers[topic] = consumer // cache the consumer
+            const consume = async (topic) => {
+                await consumer.connect() // connect a Kafka client
+                const p = {topic}
+                p.fromBeginning = false
+                await consumer.subscribe(p) // subscribe to the topic
+                console.log("Listening to: " + topic)
+                const admin = kafka.admin()
+                await admin.connect()
+                await admin.resetOffsets({ groupId, topic }) // reset the offset to "latest" (i.e., ignore old messages)
+                await admin.disconnect()
+                consumer.run({
+                    eachMessage: ({message}) => { // forward the message to the socket
+                        io.to(topic).emit(topic, message.value.toString()) // broadcast the message to the room
+                        // socket.emit(topic, message.value.toString())
+                    }
+                })
+            }
+            consume(topic).catch((err) => { console.error(err) })
+        }
+        if (topic === prevTopic) return // if the topic is the same as before do nothing
+        if (prevTopic) {
+            socket.leave(prevTopic)
+            console.log("Leaving: " + topic)
+        }
+        socket.join(topic)
+        console.log("Joining: " + topic)
         prevTopic = topic
-        if (typeof consumer !== "undefined") { // when changing the topic, remove the previous Kafka client (if any)
-            console.log('Kafka client disconnected');
-            consumer.disconnect() // disconnect the kafka consumer
-        }
-        console.log("Received a new topic: " + topic)
-        const groupId = topic + ".group." + uuid.v4()
-        console.log("Registering a new Kafka consumer with group: " + groupId)
-        consumer = kafka.consumer({groupId})
-        const consume = async (topic) => {
-            await consumer.connect() // connect a Kafka client
-            const p = {topic}
-            p.fromBeginning = false
-            await consumer.subscribe(p) // subscribe to the topic
-            const admin = kafka.admin()
-            await admin.connect()
-            await admin.resetOffsets({ groupId, topic }) // rest the offset to "latest"
-            await admin.disconnect()
-            consumer.run({
-                eachMessage: ({message}) => { // forward the message to the socket
-                    socket.emit(topic, message.value.toString())
-                }
-            })
-        }
-        consume(topic).catch((err) => { console.error(err) })
-    })
-    socket.on('disconnect', function() {
-        console.log('Socket disconnected')
-        if (typeof consumer !== "undefined") { // when changing the topic, remove the previous Kafka client (if any)
-            console.log('Kafka client disconnected')
-            consumer.disconnect() // disconnect the kafka consumer
-        }
     })
 })
-server.listen(process.env.PROXY_SERVER_PORT_INT, process.env.PROXY_SERVER_ADDRESS, function () {
+server.listen(process.env.PROXY_SERVER_PORT_INT, '0.0.0.0', function () {
     console.log('Node Server listening on port ' + process.env.PROXY_SERVER_PORT_INT)
 })
