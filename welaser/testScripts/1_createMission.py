02cad53a8566b04724623465a37e79231960ef82
w4bo <you@example.com>
2022-11-15 14:23:39 +0000

    fix the tests

--- welaser/testScripts/1_createMission.py
+++ welaser/testScripts/1_createMission.py
@@ -115,7 +115,7 @@ assert count1 > 0, "No document found"
 ###############################################################################
 today = datetime.today().strftime('%Y-%m-%d')
 NODE_URL = "http://{}:{}/api/".format(conf["IP"], conf["WEB_SERVER_PORT_EXT"])
-for url in ['download/{}/{}/{}/{}/{}/{}'.format(domain, 'AgriFarm', today, today, 'foo', 'foo'),
+for url in ['download/{}/{}/{}/{}/{}/{}'.format(domain, 'AgriFarm', today, today, '1', '1000'),
             'entitytypes/{}'.format(domain),
             'entities/{}'.format(domain),
             'entity/{}/{}'.format(domain, domain)]:
