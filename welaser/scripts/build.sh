54addf3793a254e177b635be64efb7fdd45eca7b
w4bo <you@example.com>
2022-11-21 08:50:37 +0000

    remove useless install

--- welaser/scripts/build.sh
+++ welaser/scripts/build.sh
@@ -4,6 +4,5 @@ set -exo
 # docker builder prune --all
 # docker build -t replay-executor ./service-replay
 # docker pull ghcr.io/w4bo/welaser-robot:master
-sudo apt install inotify-tools -y
 docker-compose build
 docker-compose --file kafka-docker/docker-compose.yml --env-file .env build
