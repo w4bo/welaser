b061957c8572c68be7cf63ba02d141ca0e99a3af
w4bo <you@example.com>
2022-11-22 08:45:44 +0000

    make the test more resilient

--- welaser/testScripts/2_ftp.py
+++ welaser/testScripts/2_ftp.py
@@ -1,12 +1,17 @@
 from dotenv import dotenv_values
 from ftplib import FTP
+import time
 
 conf = dotenv_values("../.env")
 ftp = FTP()
 ftp.connect(conf["IMAGESERVER_IP"], int(conf["IMAGESERVER_PORT_FTP21_EXT"]))
 ftp.login(conf["IMAGESERVER_USER"], conf["IMAGESERVER_PWD"])
 ftp.cwd('/data')
+i = 20
 dir_list = []
-ftp.dir(dir_list.append)
+while i > 0 and len(dir_list) < 2:
+    time.sleep(1)
+    dir_list = []
+    ftp.dir(dir_list.append)
 assert len(dir_list) > 2, "Not enough images: " + str(dir_list)
 ftp.quit()
