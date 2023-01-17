45d7ccf65aa47fbd487d76d5a2f94e414d8ac10a
w4bo <you@example.com>
2023-01-17 14:33:34 +0000

    workin on the device

--- welaser/service-dashboard/public/js/devicedata.js
+++ welaser/service-dashboard/public/js/devicedata.js
@@ -1,14 +1,25 @@
-const statistics = {
+const devicedata = {
     template: `
         <v-row justify="center">
             <v-col cols="3"><chartcount></chartcount></v-col>
             <v-col cols="3"><chartdelay></chartdelay></v-col>
         </v-row>`,
     data() {
-        return {}
+        return {
+            devices: {}
+        }
     },
-    components: {
-        chartcount: chartcount,
-        chartdelay: chartdelay
+    mounted() {
+        const tis = this
+        axios.get(utils.nodeurl + `/api/download/${utils.agrifarm}/Device/${moment().subtract(1, "days").format('x')}/${moment().format('x')}/0/1000000`)
+            .then(entities => {
+                tis.devices = entities.data
+                console.log(tis.devices)
+            })
     }
 }
+
+/**
+ * GET ALL DEVICES FROM THE AGRIFARM,
+ * FOR EACH DEVICE PLOT THE STORED DATA
+ */
