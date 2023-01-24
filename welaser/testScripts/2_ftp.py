1c5fcbb64f73918a659f4133a82b9c1999be4939
Matteo Francia <m.francia@unibo.it>
2023-01-24 09:40:04 +0000

    working on ftp

--- welaser/testScripts/2_ftp.py
+++ welaser/testScripts/2_ftp.py
@@ -6,7 +6,8 @@ conf = dotenv_values("../.env")
 ftp = FTP()
 ftp.connect(conf["IMAGESERVER_IP"], int(conf["IMAGESERVER_PORT_FTP21_EXT"]))
 ftp.login(conf["IMAGESERVER_USER"], conf["IMAGESERVER_PWD"])
-ftp.cwd('/data')
+# ftp.cwd('data')
+# ftp.cwd('/home/' + conf["IMAGESERVER_USER"])
 i = 20
 dir_list = []
 while i > 0 and len(dir_list) < 2:
