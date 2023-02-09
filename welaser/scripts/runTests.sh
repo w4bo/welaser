e3c76f301f18d87503ed989e31a1d70ae324dff8
w4bo <you@example.com>
2023-02-09 17:37:42 +0000

    fix the path

--- welaser/scripts/runTests.sh
+++ welaser/scripts/runTests.sh
@@ -4,5 +4,5 @@ set -exo
 cd testScripts
 source venv/bin/activate
 for FILE in *.py; do echo "$FILE"; python "$FILE"; done
-for FILE in *.sh; do echo "$FILE"; "./${FILE}"; done
+for FILE in *.sh; do echo "$FILE"; source "./${FILE}"; done
 cd -
