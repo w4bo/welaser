5e886dd313a6717718a1adc61e7816e5d0435d6d
w4bo <you@example.com>
2023-02-20 13:52:37 +0000

    fix docker compose

--- welaser/service-kafkaproxy/index.test.js
+++ welaser/service-kafkaproxy/index.test.js
@@ -10,12 +10,15 @@ test('test dotenv', async () => {
 test('test roundtrip', async () => {
     const topic = "test.kafkaproxy"
     const remoteSocket = require('socket.io-client')(`http://${process.env.PROXY_IP}:${process.env.PROXY_PORT_EXT}`)
+    console.log("Connected...")
     let c = 0
     remoteSocket.on(topic, _ => c++) // register a subscriber to the kafka proxy
     remoteSocket.emit("newtopic", topic) // notify the KafkaProxy to subscribe to a Kafka topic
     const retry = 10
     for (let i = 0; i < retry && c === 0; i++) { // ... publish on the same topic (i.e., KafkaProxy sends a message on Kafka)
+        console.log("Waiting...")
         await new Promise((r) => setTimeout(r, (timelimit * 0.98) / retry)) // ... wait
+        console.log("Publishing...")
         remoteSocket.emit("publish", {topic: topic, data: {}})
     }
     remoteSocket.close()
