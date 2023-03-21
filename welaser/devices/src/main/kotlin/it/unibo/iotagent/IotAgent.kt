5dfac8a3208c8627ab40e9716e259eead7094105
w4bo <you@example.com>
2023-03-21 16:18:53 +0000

    fix encoding order

--- welaser/devices/src/main/kotlin/it/unibo/iotagent/IotAgent.kt
+++ welaser/devices/src/main/kotlin/it/unibo/iotagent/IotAgent.kt
@@ -107,7 +107,7 @@ class IOTA {
                     khttp.async.post(
                             "${ORION_URL}op/update?options=keyValues",
                             mapOf("Content-Type" to "application/json"),
-                            data = payload.toString().replace("%", "%25").replace("=", "%3D"),
+                            data = payload.toString().replace("=", "%3D"),
                             onResponse = {
                                 if ((statusCode / 100) != 2) {
                                     println("Error [$statusCode] $text $payload")
