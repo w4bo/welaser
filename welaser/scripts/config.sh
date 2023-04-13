214d5f467021ea4d2c443dea1c6a2f94e952dd81
w4bo <you@example.com>
2023-04-13 14:24:45 +0000

    fix the mqtt configuration

--- welaser/scripts/config.sh
+++ welaser/scripts/config.sh
@@ -40,6 +40,13 @@ ln .env service-dashboard/.env
 ln .env service-kafkaproxy/.env
 
 # Mosquitto
-if [ ! -f "mosquitto/config/pwfile" ]; then
-    cp mosquitto/config/pwfile.example mosquitto/config/pwfile
+cd mosquitto/config
+if [ ! -f "pwfile" ]; then
+    cp pwfile.example pwfile
 fi
+if [ -f "certs/rootCA.crt" ]; then
+    cp mosquitto-tls.conf mosquitto.conf
+else
+    cp mosquitto-plain.conf mosquitto.conf
+fi
+cd -
