d7d98232f9cac699efb7a14619a92d6dc2e20153
Your Name <you@example.com>
2023-04-26 10:55:54 +0000

    update the services

--- welaser/scripts/submodule.sh
+++ welaser/scripts/submodule.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-set -xo
-git submodule update --init --recursive --force
-git submodule foreach git pull origin master
-git submodule foreach git pull origin main
-git pull --recurse-submodules
+# git submodule update --init --recursive --force
+# git submodule foreach git pull origin master
+# git submodule foreach git pull origin main
+# git pull --recurse-submodules
+for folder in "service-mapbuilder" "service-missionplanner" "service-missionsupervisor" "welaser-datamodels"; do
+  echo "$folder"
+  cd "$folder"
+  git pull origin main
+  git pull origin master
+  cd -
+done
