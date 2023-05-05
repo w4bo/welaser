8c354ac65f9f04140675625ed7e0a5e312bcae28
w4bo <you@example.com>
2023-05-05 08:04:04 +0000

    update mapbuilder and missionplanner

--- welaser/scripts/submodule.sh
+++ welaser/scripts/submodule.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# git submodule update --init --recursive --force
+git submodule update --init --recursive --force
 # git submodule foreach git pull origin master
 # git submodule foreach git pull origin main
 # git pull --recurse-submodules
