c4af90c31b7ab740e6277923e2974512392458b2
Your Name <you@example.com>
2023-10-16 14:44:36 +0000

    remove the --remove-orphans flag

--- welaser/stop.sh
+++ welaser/stop.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-docker compose -f docker-compose.kafka.yml down --remove-orphans
-docker compose down --remove-orphans
+docker compose -f docker-compose.yml -f docker-compose.kafka.yml down --remove-orphans
 # docker stack rm kafka welaser
 rm -f logs/*.txt
